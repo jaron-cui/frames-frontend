7177ef67e5a4a47f07ae590c41f5c321d9845f0b
the0919 <theodonacik@gmail.com>
2022-03-27 18:43:48 -0400

    Basic Websocket working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import SockJsClient from 'react-stomp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Client extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lastMessage: "",
+			name: "Theo",
+		}
+	};
+
+	onMessage = (message) => {
+		console.log(message);
+		this.setState({lastMessage : message.content});
+	}
+	sendMessage = () => {
+        this.clientRef.sendMessage('/app/hello', JSON.stringify({
+            name: this.state.name,
+        }));
+    };
+	handleChange = async function(event) {
+		await this.setState({name : event.target.value});
+		this.sendMessage()
+	}
+	render() {
+
+		return (
+			<div>
+				<SockJsClient url={ "http://localhost:8080/gs-guide-websocket" }
+				topics={["/topic/response"]}
+				onConnect={ () => { this.sendMessage() }}
+				onDisconnect={() => {
+					console.log("Disconnected");
+				}}
+				onMessage={ (msg) => {this.onMessage(msg)} } 
+				ref={(client) => {this.clientRef = client}}
+				/>
+				<input type="text" 
+				name="name"
+				value={this.state.name}
+				onChange={ (event) => {this.handleChange(event)}}
+				/>
+				<span>{ this.state.lastMessage }</span>
+			</div>
+		  );
+	}
+
 }
 
-export default App;
+export default Client;

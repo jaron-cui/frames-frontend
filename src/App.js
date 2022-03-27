6536417dfd9e3ad3e75967cfcdf9e864a4486561
the0919 <theodonacik@gmail.com>
2022-03-27 19:01:14 -0400

    basic websocket + jaron's animation start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
 import React, { useState, useEffect } from "react";
 import SockJsClient from 'react-stomp';
+import Canvas from "./Canvas";
+
+
+//const worker = new Worker("worker.js");
+//const canvas = <Canvas name="canvas" width={400} height={300} worker={worker}/>;
+//const animator = new Animator("canvas");
 
 class Client extends React.Component {
 	constructor(props) {
@@ -24,28 +32,29 @@ class Client extends React.Component {
 		this.sendMessage()
 	}
 	render() {
-
 		return (
 			<div>
-				<SockJsClient url={ "http://localhost:8080/gs-guide-websocket" }
-				topics={["/topic/response"]}
-				onConnect={ () => { this.sendMessage() }}
-				onDisconnect={() => {
-					console.log("Disconnected");
-				}}
-				onMessage={ (msg) => {this.onMessage(msg)} } 
-				ref={(client) => {this.clientRef = client}}
-				/>
-				<input type="text" 
-				name="name"
-				value={this.state.name}
-				onChange={ (event) => {this.handleChange(event)}}
-				/>
-				<span>{ this.state.lastMessage }</span>
+        		<Canvas name="canvas" width={400} height={300} />
+				<div>
+					<SockJsClient url={ "http://localhost:8080/gs-guide-websocket" }
+					topics={["/topic/response"]}
+					onConnect={ () => { this.sendMessage() }}
+					onDisconnect={() => {
+						console.log("Disconnected");
+					}}
+					onMessage={ (msg) => {this.onMessage(msg)} } 
+					ref={(client) => {this.clientRef = client}}
+					/>
+					<input type="text" 
+					name="name"
+					value={this.state.name}
+					onChange={ (event) => {this.handleChange(event)}}
+					/>
+					<span>{ this.state.lastMessage }</span>
+				</div>
 			</div>
 		  );
 	}
-
 }
 
 export default Client;
